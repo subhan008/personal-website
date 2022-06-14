cb4c1f09b031cc313cafe74b905624a3dfe331bc
subhan ibrahim <subhanibrahim07@gmail.com>
2022-06-14 13:59:53 +0530

    personal website

---
+++ script.js
@@ -0,0 +1,66 @@
+var nameError = document.getElementById('name-error')
+var emailError= document.getElementById('email-error')
+var mobileError= document.getElementById('mobile-error')
+var messageError= document.getElementById('message-error')
+var submitError= document.getElementById('submit-error')
+
+function validateName() {
+   var name = document.getElementById('name').value;
+
+   if(name.length <3){
+      nameError.innerHTML ='Name required';
+      return false; 
+   }
+   if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+      nameError.innerHTML='write full name';
+      return false;
+   }
+   nameError.innerHTML = '';
+   return true;
+}
+function validateMobile(){
+   var mobile = document.getElementById('number').value;
+   if(mobile.length==0){
+      mobileError.innerHTML='Mobile no required'
+      return false;
+   }
+   if(mobile.length!==10){
+      mobileError.innerHTML='mobile no should be 10 digits'
+      return false;
+   }
+   if (!mobile.match(/^[0-9]{10}$/)) {
+    mobileError.innerHTML='Mobile no required'
+    return false;      
+   }
+   mobileError.innerHTML= '';
+   return true;
+}
+
+function validateEmail(){
+   var email = document.getElementById('email').value
+   if(email.length==0){
+      emailError.innerHTML = 'Email required'
+      return false;
+   }
+   if (!email.match(/^[0-9]{10}$/)) {
+      emailError.innerHTML='Email invalid'
+      return false;      
+     }
+     emailError.innerHTML= '';
+     return true;
+}
+function validateMessage(){
+   var message = document.getElementById('message').value
+   var required = 30;
+   var left = required -message.length;
+   if(left > 0){
+      messageError.innerHTML = left + 'More character required' 
+      return false;
+   }
+     
+     messageError.innerHTML= '';
+     return true;
+}
+
+
+
